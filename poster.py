f88935d90e09e3381cb7ce52470a1daa324bdb9f
James Pickar <jamieppickar@gmail.com>
2020-07-31 14:37:12 -0400

    New EDP

--- poster.py
+++ poster.py
@@ -36,6 +36,36 @@ def server_is_active(url: str):
     return state_of_the_server
 
 
+def enumerate_duplicate_paths(start_path: Path):
+    final_path: Path
+    if not start_path.exists():
+        final_path = start_path
+    else:
+        parent = start_path.parent
+        suffix = "".join(start_path.suffixes)
+        test_path = start_path.with_suffix('').name
+
+        ends_in_a_number: bool = True
+
+        try:
+            iteration_number = int(test_path.split()[-1])
+
+        except (ValueError, IndexError):
+            iteration_number = 1
+            ends_in_a_number = False
+
+        print("    Generating name...")
+        while ((parent / test_path).with_suffix(suffix)).exists():
+            if ends_in_a_number:
+                test_path = test_path.split()[:-1][0]
+            test_path += " " + str(iteration_number + 1)
+            ends_in_a_number = True
+            iteration_number += 1
+
+        final_path = (parent / test_path).with_suffix(suffix)
+    return final_path
+
+
 # "Procedural" functions
 def determine_txts(input_dir):
     input_path = Path(input_dir)
@@ -87,12 +117,42 @@ def analyze_txts(txt_file_paths: list, url: str, final_format: str):
     for txt_file_path in txt_file_paths:
         data = open(str(txt_file_path), "rb").read()
         response = requests.post(url, headers=headers, data=data)
-        analyzed_txts.update([(txt_file_path, response)])
+        analyzed_txts.update([(txt_file_path.name, response)])
     return analyzed_txts
 
 
+def determine_output_path(input_dir: str, output_dir: str, new_dir: bool):
+    final_output_path: Path
+    if output_dir:
+        preliminary_output_path = Path(output_dir)
+        if not preliminary_output_path.exists():
+            print(output_dir, "does not exist. Please enter an exist output directory if you wish to manually select "
+                              "one.")
+            exit(1)
+        if new_dir:
+            # do enumeration
+            final_output_path = Path("")
+        else:
+            final_output_path = preliminary_output_path
+    else:
+        if new_dir:
+            # do enumeration
+            final_output_path = Path("")
+        else:
+            final_output_path = Path(input_dir)
+    return final_output_path
+
+
+def touch_files(analysis_list: list, directory_path: Path, mode: int):
+    for analysis_path in analysis_list:
+        created_path = directory_path.joinpath(analysis_path)
+        created_path.touch(mode)
+        # analysis_list[analysis_path].key
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Sends all .txt files trough the sa-engine.")
+    parser = argparse.ArgumentParser(description="Sends all .txt files trough the sa-engine and writes the analysis "
+                                                 "to a new set of txts.")
     parser.add_argument("input", metavar="Input directory path", type=str, help="Path of directory with .TXTs to "
                                                                                 "extract.")
     parser.add_argument("address", metavar="Server address", type=str, help="Address of server on which the sa-engine "
@@ -101,9 +161,16 @@ if __name__ == "__main__":
                                                                     "is running. Defaults to 8080.")
     parser.add_argument("-f", metavar="Output format", type=str, help="The output type of the request, either xml or "
                                                                       "json. Defaults to xml")
+    parser.add_argument("-nd", action="store_true", help="Places analyzed text files into new directory with same "
+                                                         "name as original directory but with 'analyzed txts' on the "
+                                                         "end(Defaults to false).")
+    parser.add_argument("--output", metavar="Output directory path", type=str, help="Path of directory to place "
+                                                                                    "analyzed txts into files to ("
+                                                                                    "Defaults to same as input).")
     args = parser.parse_args()
 
     files = determine_txts(args.input)
     output_format = determine_format(args.f)
     output_url = determine_url(args.address, args.p)
     analysis = analyze_txts(files, output_url, output_format)
+    output_path = determine_output_path(args.input, args.output, args.nd)
--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-import requests
-try:
-    response = requests.get('http://ec2-35-174-241-30.compute-1.amazonaws.com:8080/management/health')
-except requests.exceptions.ConnectionError:
-    print("Connection failed")
-except requests.exceptions.Timeout:
-    print("Connection Timeout")
-except requests.exceptions.TooManyRedirects:
-    print("Too Many Redirects")
-except requests.exceptions.RequestException:
-    print("Other Exception")
-else:
-    print("success")
+print("empty")
